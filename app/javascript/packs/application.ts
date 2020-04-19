624f09ab31bf3ac1b4df28fd0b20fec460083ea8
Joshua Hunsche Jones <joshua@hunschejones.com>
2020-04-19 12:45:19 -0700

    Switch to TypeScript w/ linting tools

---
+++ app/javascript/packs/application.ts
@@ -0,0 +1,8 @@
+import * as ActiveStorage from "@rails/activestorage";
+import Rails from "@rails/ujs";
+import * as Channels from "channels";
+import Turbolinks from "turbolinks";
+
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
