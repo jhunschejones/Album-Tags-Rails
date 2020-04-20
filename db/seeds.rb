4aa8c89a23aa1f769eeeb1d33a62724133b367ca
Joshua Hunsche Jones <joshua@hunschejones.com>
2020-04-20 08:13:05 -0700

    Add seed user for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# path to your application root.
+APP_ROOT = File.expand_path('..', __dir__)
+
+FileUtils.chdir APP_ROOT do
+  system '. ./tmp/.env'
+end
+
+if User.count == 0
+  User.create(name: "Joshua", email: ENV['EMAIL_USERNAME'], password: ENV['DEV_PASSWORD'], password_confirmation: ENV['DEV_PASSWORD'], confirmed_at: Time.now.utc, site_role: "site_admin")
+end
