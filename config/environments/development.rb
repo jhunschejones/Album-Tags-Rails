21b3b03692cd2ea058b63077ea06e3de31554f04
Joshua Hunsche Jones <joshua@hunschejones.com>
2020-05-12 08:26:34 -0700

    Update to rails 6.0.3
    + devise_invitable, lockbox

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-    # Use the lowest log level to ensure availability of diagnostic information
+  # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
 
