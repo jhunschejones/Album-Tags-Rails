016121406efec0013e21cd750dd7c6c858b2445e
Joshua Hunsche Jones <joshua@hunschejones.com>
2020-05-26 11:18:07 -0700

    Clean up orphaned tags right away

--- app/models/album_tag.rb
+++ app/models/album_tag.rb
@@ -2,4 +2,12 @@ class AlbumTag < ApplicationRecord
   belongs_to :album
   belongs_to :tag
   belongs_to :user
+
+  after_destroy :remove_tag_if_orphaned
+
+  private
+
+  def remove_tag_if_orphaned
+    tag.destroy unless tag.album_tags.present?
+  end
 end
