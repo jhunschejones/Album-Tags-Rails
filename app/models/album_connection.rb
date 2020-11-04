a38c900beda6563d95f282a955724d36916c797e
Joshua Hunsche Jones <joshua@hunschejones.com>
2020-11-04 15:07:47 -0800

    Verify album connections are unique from both sides

--- app/models/album_connection.rb
+++ app/models/album_connection.rb
@@ -2,4 +2,19 @@ class AlbumConnection < ApplicationRecord
   belongs_to :parent_album, :class_name => :Album
   belongs_to :child_album, :class_name => :Album
   belongs_to :user
+
+  validate :connection_not_duplicate
+
+  private
+
+  def connection_not_duplicate
+    connection_already_esists = AlbumConnection
+      .where(parent_album_id: parent_album_id, child_album_id: child_album_id)
+      .or(AlbumConnection.where(parent_album_id: child_album_id, child_album_id: parent_album_id))
+      .exists?
+
+    if connection_already_esists
+      errors.add(:parent_album_id, "id #{parent_album_id} is already connected to album id #{child_album_id}")
+    end
+  end
 end
